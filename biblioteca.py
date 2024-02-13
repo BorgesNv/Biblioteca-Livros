99e61d82bd7cdf5f2f5135caa406bc49ae78f7e4
Igor Borges <159856124+BorgesNv@users.noreply.github.com>
2024-02-13 08:26:58 -0300

    Corrigido Autor Livro2

--- biblioteca.py
+++ biblioteca.py
@@ -1,7 +1,7 @@
 from ex1 import Livro
 
 livro1 = Livro('O Filho de Netuno', 'Rick Riordan', 2011)
-livro2 = Livro('O Caminho Jedi', 'LucasFilms', 2007)
+livro2 = Livro('O Caminho Jedi', 'LucasFilm', 2007)
 livro3 = Livro('God of War I', 'Sony Playstation', 2022)
 
 livro1.emprestar()
